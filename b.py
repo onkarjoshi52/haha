b14365ffec03b59b6857e46abaae45c6a0376bc9
onkarjoshi52 <58779778+onkarjoshi52@users.noreply.github.com>
2020-02-27 11:07:38 +0530

    Add files via upload

--- b.py
+++ b.py
@@ -19,15 +19,6 @@ def blink(num):
         
 
 while True:
-    try:
-        f=open('logs/log'+str(j)+'.txt',"r")
-        if f:
-            j+=1
-    except:
-        open('logs/log'+str(j)+'.txt',"w")
-        print("File Created")
-        break
-while True:
     
     input_state = GPIO.input(4) #Read and store value of input to a variable
     if input_state == False:     #Check whether pin is grounded
@@ -39,58 +30,60 @@ while True:
            bytesize=serial.EIGHTBITS,
            timeout=1
        )
-       
-       
-       f1=open('logs/log'+str(j)+'.txt',"a+")
-       x=ser.read(994)
-       f1.write(x)
-       print (x)
-       f1.close()
+       try:
+           f=open('logs/log'+str(j)+'.txt',"r")
+           if f:
+               j+=1
+       except:
+           open('logs/log'+str(j)+'.txt',"w")
+
+           while True:
+
+                   f1=open('logs/log'+str(j)+'.txt',"a+")
+                   x=ser.read(994)
+                   f1.write(x)
+                   f1.close()
+                   if GPIO.input(4) == GPIO.HIGH:
+                       break
+           
+           
+    
        
            
     else:
         if GPIO.input(14) == GPIO.HIGH:
-            print("Button was pushed!")
             time.sleep(1)
             n=i%8
             if n == 0:
                 z=9600
-                print(z)
                 blink(n+1)
                 
             elif n == 1:
                 z=19200
-                print(z)
                 blink(n+1)
                 
             elif n == 2:
                 z=38400
-                print(z)
                 blink(n+1)
                 
             elif n == 3:
                 z=57600
-                print(z)
                 blink(n+1)
                 
             elif n == 4:
                 z=115200
-                print(z)
                 blink(n+1)
                 
             elif n == 5:
                 z=230400
-                print(z)
                 blink(n+1)
                 
             elif n == 6:
                 z=460800
-                print(z)
                 blink(n+1)
                 
             elif n == 7:
                 z=921600
-                print(z)
                 blink(n+1)
                 
             i=i+1
